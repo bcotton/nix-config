320f890044203891e15cd46e24595a8698f5d2be
Bob Cotton <bob.cotton@gmail.com>
2025-04-06 16:21:39 -0600

    speed up intial testing

--- hosts/darwin/bobs-laptop/default.nix
+++ hosts/darwin/bobs-laptop/default.nix
@@ -47,7 +47,7 @@ in {
     };
 
     homebrew = {
-      enable = true;
+      enable = false;
       # updates homebrew packages on activation,
       # can make darwin-rebuild much slower (otherwise i'd forget to do it ever though)
       onActivation.upgrade = true;
