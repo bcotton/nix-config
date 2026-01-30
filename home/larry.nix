58a8b679366fbe967aed8d6a7f178e58689f00e3
Bob Cotton <bob.cotton@gmail.com>
2026-01-30 06:28:58 -0700

    just fmt

--- home/larry.nix
+++ home/larry.nix
@@ -93,7 +93,7 @@
       # Ensure systemd user session env vars are set
       export XDG_RUNTIME_DIR="/run/user/$(id -u)"
       export DBUS_SESSION_BUS_ADDRESS="unix:path=$XDG_RUNTIME_DIR/bus"
-      
+
       # tea completion if available (source the file directly to avoid "Fetching" output)
       [[ -f ~/.config/tea/autocomplete.zsh ]] && PROG=tea _CLI_ZSH_AUTOCOMPLETE_HACK=1 source ~/.config/tea/autocomplete.zsh
     '';
